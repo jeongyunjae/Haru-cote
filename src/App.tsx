f52882b0fa1d52a93d457756cbebd1642fbd3b31
jeongyunjae <yunjae2295@naver.com>
2024-01-11 13:29:38 +0900

    feat: 레이아웃 및 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import Icon from './components/Icon/icon'
+import Layout from './components/Layout'
+import PicksPage from './pages/PicksPage'
+import SettingsPage from './pages/SettingsPage'
+import TimerPage from './pages/TimerPage'
+import NoMatchPage from './pages/NoMatchPage'
 
 function App() {
   return (
-    <>
-      <div></div>
-      <Icon name='Logo' />
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<PicksPage />} />
+        <Route path='settings' element={<SettingsPage />} />
+        <Route path='timer' element={<TimerPage />} />
+
+        <Route path='*' element={<NoMatchPage />} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Footer() {
+  return (
+    <div>
+      <Text>Watch</Text>
+    </div>
+  )
+}
+
+const Text = styled.p`
+  font-size: 16px;
+`
