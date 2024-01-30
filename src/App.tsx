b407f431e36363a32d1134558386c5f901eb5234
jeongyunjae <yunjae2295@naver.com>
2024-01-30 14:22:25 +0900

    fix: 기본 설정 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
-import Icon from './components/Icon/icon'
 import Layout from './components/Layout'
-import PicksPage from './pages/PicksPage'
 import SettingsPage from './pages/SettingsPage'
 import NoMatchPage from './pages/NoMatchPage'
 import WatchPage from './pages/WatchPage'
+import LandingPage from './pages/LandingPage'
+import PickPage from './pages/PickPage'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<PicksPage />} />
+        <Route index element={<LandingPage />} />
+        <Route path='pick' element={<PickPage />} />
         <Route path='settings' element={<SettingsPage />} />
         <Route path='watch' element={<WatchPage />} />
 
