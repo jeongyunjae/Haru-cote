fbc3948dae1b89aecb21a03bf2eea6e58960cbde
jeongyunjae <yunjae2295@naver.com>
2024-03-10 01:19:32 +0900

    feat: 데이터 하드코딩 및 array 함수 추가

---
+++ src/data/community.ts
@@ -0,0 +1,10 @@
+export type PeopleNameType = '윤재' | '지은' | '사라' | '가윤' | '진욱' | '민지'
+
+export const PEOPLE_NAMES: PeopleNameType[] = [
+  '윤재',
+  '지은',
+  '사라',
+  '가윤',
+  '진욱',
+  '민지',
+]
---
+++ src/utils/array.ts
@@ -0,0 +1,17 @@
+/**
+    @description 배열을 쉼표 기준으로 하나의 문자열로 생성
+    @example [123,1256,'abc'] -> '123,1256,abc'
+   */
+export function addCommaForArray(values: (string | number)[]) {
+  const data = values.join(',')
+  return data
+}
+
+export type NonEmptyArray<T> = [T, ...T[]]
+/**
+    @description 배열에 1개 이상의 요소가 있는지 검사합니다
+    @example [123] => true, [] => false
+   */
+export function isNonEmptyArray<T>(array: T[]): array is NonEmptyArray<T> {
+  return array.length >= 1
+}
