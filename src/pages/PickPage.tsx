2ed236f13b3a2f8e9866395545641fbdcc168091
jeongyunjae <yunjae2295@naver.com>
2024-03-12 03:04:30 +0900

    fix: 문제 선택 로직 개선 1. 커스텀 훅으로 전환 2. setState 함수로 변경

--- src/pages/PickPage.tsx
+++ src/pages/PickPage.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -6,43 +5,21 @@ import StartPickStep from '../components/StartPickStep'
 import ShowPickStep from '../components/ShowPickStep'
 import { useFunnel } from '../components/Funnel/useFunnel'
 import Button from '../components/Button/Button'
-import usePickStore from '../modules/pickStore/usePickStore'
-import practiceData from '../data/practiceTestData.json'
+import { usePick } from './hooks/usePick'
 
 export type levelType = { value: number; isContentOpen: boolean }
 
 export default function PickPage() {
   const navigate = useNavigate()
-  const { Funnel, Step, currentStep, setStep } = useFunnel('문제 선정하기')
-  const { levelData } = usePickStore()
-  const [problemIdList, setProblemIdList] = useState<number[]>([])
-  const sumOfLevels = Object.values(levelData).reduce(
-    (acc, value) => acc + value,
-    0
-  )
-
-  const handleChoiceButtonClick = useCallback(() => {
-    let subset: number[] = []
-
-    Object.values(levelData).forEach((count, _i) => {
-      const filteringLevel = practiceData
-        .filter(({ level, isSolved }) => level === _i + 1 && !isSolved)
-        .map(({ problemNumber }) => problemNumber)
-
-      const shuffled = [...filteringLevel]
-      for (let i = shuffled.length - 1; i >= 0; i--) {
-        const randomIndex = Math.floor(Math.random() * (i + 1))
-        ;[shuffled[i], shuffled[randomIndex]] = [
-          shuffled[randomIndex],
-          shuffled[i],
-        ]
-      }
-      subset = [...subset, ...shuffled.slice(0, count)]
-    })
-
-    setProblemIdList([...subset])
-    setStep('확정하기')
-  }, [levelData, practiceData, setProblemIdList, setStep])
+  const { Funnel, Step, currentStep, handleStep } = useFunnel('문제 선정하기')
+  const {
+    problemIdList,
+    sumOfLevels,
+    handleChoiceButtonClick,
+    WEEK_PROBLEM_COUNT,
+  } = usePick({
+    handleStep,
+  })
 
   return (
     <PageWrapper>
@@ -68,7 +45,7 @@ export default function PickPage() {
               fullWidth
               onClick={handleChoiceButtonClick}
               theme='fill_normal'
-              disabled={sumOfLevels !== 5}
+              disabled={sumOfLevels !== WEEK_PROBLEM_COUNT}
             />
           ) : (
             <>
@@ -76,7 +53,7 @@ export default function PickPage() {
                 label='뒤로가기'
                 size='medium'
                 theme='soft_mono'
-                onClick={() => setStep('문제 선정하기')}
+                onClick={() => handleStep('문제 선정하기')}
                 halfWidth
               />
               <Button
@@ -98,41 +75,48 @@ const PageWrapper = styled.main`
   width: 100%;
   height: calc(100vh - 50px - 82px);
   position: relative;
-  align-items: center;
-  justify-content: center;
   display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const PickWrapper = styled.section`
-  position: relative;
   width: 326px;
   height: calc(100% - 80px);
+  position: relative;
+  padding: 18px 12px;
   box-sizing: border-box;
-  padding: 0px 12px;
-  border-radius: 16px;
+  background-color: var(--gray100);
+  border-radius: 20px;
 `
 
 const TitleWrapper = styled.div`
   width: 100%;
-  box-sizing: border-box;
+  height: 40px;
   position: relative;
-
-  height: 50px;
-
+  box-sizing: border-box;
   display: flex;
   justify-content: center;
   align-items: center;
 
   font-size: var(--h3);
-  line-height: var(--h3LineHeight);
   font-weight: var(--bold);
+  line-height: var(--h3LineHeight);
 `
 
 const MainWrapper = styled.div`
   /* 40px: title, 60px: footer */
-  height: calc(100% - 50px - 60px);
+  height: calc(100% - 40px - 60px);
   max-height: 500px;
+  margin-bottom: 12px;
+  box-sizing: border-box;
   overflow-y: auto;
+
+  scrollbar-width: none; /* Firefox에 대한 스크롤바 숨김 */
+  -ms-overflow-style: none; /* IE 및 Edge에 대한 스크롤바 숨김 */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome 및 Safari에 대한 스크롤바 숨김 */
+  }
 `
 
 const FooterWrapper = styled.div`
