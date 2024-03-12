7e98dca18ee1cc39d807ee336f1eeaf52db0674e
jeongyunjae <yunjae2295@naver.com>
2024-03-13 06:01:16 +0900

    fix: 사용하지 않는 파일제거

--- src/components/DonePickStep.tsx
+++
@@ -1,7 +0,0 @@
-export type DonePickStepProps = {
-  goToStart: () => void
-}
-
-export default function DonePickStep({ goToStart }: DonePickStepProps) {
-  return <div onClick={goToStart}>처음으로 돌아가기</div>
-}
