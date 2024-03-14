7572611b3881c7a730285074bf9e1173e36be096
jeongyunjae <yunjae2295@naver.com>
2024-03-15 01:37:11 +0900

    fix: 배포테스트 완료

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: window.location.origin,
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3080/'
+      : 'https://port-0-haru-cote-server-fi1xh2bltqnoclo.sel5.cloudtype.app/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  server: {
-    proxy: {
-      '/api': {
-        target:
-          process.env.NODE_ENV === 'development'
-            ? 'https://port-0-haru-cote-server-fi1xh2bltqnoclo.sel5.cloudtype.app/'
-            : 'https://port-0-haru-cote-server-fi1xh2bltqnoclo.sel5.cloudtype.app/',
-        changeOrigin: true,
-      },
-    },
-  },
 })
