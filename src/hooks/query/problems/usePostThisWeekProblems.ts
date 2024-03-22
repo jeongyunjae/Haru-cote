072075dee9f0ba3dcfaa296acc1c2d30199e1c3f
jeongyunjae <yunjae2295@naver.com>
2024-03-22 21:41:36 +0900

    feat: 랜딩페이지 문제 배정기능 추가

--- src/hooks/query/problems/usePostThisWeekProblems.ts
+++ src/hooks/query/problems/usePostThisWeekProblems.ts
@@ -5,16 +5,19 @@ import {
   postThisWeekProblems,
 } from '../../../lib/api/problems/postThisWeekProblems'
 import useSnackbarStore from '../../../modules/snackbar/useSnackbarStore'
+import useThisWeekProblemsQuery from './useThisWeekProblemsQuery'
 
 export default function usePostThisWeekProblems() {
   const navigate = useNavigate()
   const { openSnackbar } = useSnackbarStore()
+  const { refetch } = useThisWeekProblemsQuery()
 
   return useMutation(
     (value: PostThisWeekProblemsReq[]) => postThisWeekProblems(value),
     {
       onSuccess: async () => {
         navigate('/')
+        await refetch()
         openSnackbar('이번 주 문제가 선정되었습니다!')
       },
     }
